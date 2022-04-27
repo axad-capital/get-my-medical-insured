666fa478a5a91e2c72d8a3d7bbd1db0235c10c5c
Anthony Parrino <anthony21parrino21@gmail.com>
2022-04-27 19:43:12 -0400

    added pixel

--- client/src/components/Thanks/Thanks.js
+++ client/src/components/Thanks/Thanks.js
@@ -16,6 +16,8 @@ const Thanks = () => {
 
             <h1 style={{ textAlign: "center" }}>Check Out Your Life Insurance Quotes</h1>
             <br />
+
+            <iframe title='pixel' src="https://www.a1jkghtrk.com/?nid=1715&oid=1105&amount=AMOUNT" scrolling="no" frameborder="0" width="1" height="1"></iframe>
         </div>
     )
 }
